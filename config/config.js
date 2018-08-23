f8ee3d3e2a213926befd7f087e9c5de3b76c26f6
Unknown <carlossas_97@hotmail.com>
2018-08-23 11:41:24 -0500

    Version final 1

--- config/config.js
+++ config/config.js
@@ -1,1 +1,4 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+//GOOGLE CLIENT ID
+module.exports.CLIENT_ID = '632330140314-hom8p2st22po220p4jmmrdd7fdv8b3l0.apps.googleusercontent.com';
--- routes/login.js
+++ routes/login.js
@@ -7,6 +7,107 @@ var SEED = require('../config/config').SEED;
 var app = express();
 var Usuario = require('../models/usuario');
 
+//GOOGLE LIBRARIES
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+var CLIENT_ID = require('../config/config').CLIENT_ID;
+
+
+
+/////////////////////////////////////////AUTENTICACION CON GOOLGE/////////////////////////////////////////////
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    // const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    }
+}
+
+
+app.post('/google', async(req, res) => {
+
+    let token = req.body.token;
+
+    try {
+        // Notese que utilizo var, porque voy a necesitar usar googleUser fuera del try
+        var googleUser = await verify(token);
+    } catch (err) {
+        res.status(403).json({
+            ok: false,
+            message: "Token de google invalido",
+            errors: { message: "Token de google invalido" }
+        });
+        return;
+    }
+
+    Usuario.findOne({ email: googleUser.email }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuario',
+                errors: err
+            });
+        }
+        //USUARIO YA EXISTE EN LA BASE DE DATOS, ENTONCES:
+        if (usuarioDB) {
+            //SI EL USUARIO YA SE REGISTRO NORMALMENTE SIN USAR GOOGLE
+            if (!usuarioDB.google) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Este usuario se registro de otra manera, intente ingresar su autenticación normal',
+                });
+                //SI EL USUARIO YA SE REGISTRO CON GOOGLE SOLO SE GENERA UN NUEVO TOKEN
+            } else {
+                var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); // 4 horas
+
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token: token,
+                    id: usuarioDB._id
+                });
+
+            }
+            //SI ES LA PRIMERA VEZ QUE ENTRA, LO REGISTRAMOS EN LA BASE DE DATOS
+        } else {
+
+            var usuario = new Usuario();
+
+            usuario.nombre = googleUser.nombre;
+            usuario.email = googleUser.email;
+            usuario.img = googleUser.img;
+            usuario.google = true;
+            usuario.password = 'k mira prro';
+
+            usuario.save((err, usuarioDB) => {
+
+                var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); // 4 horas
+
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token: token,
+                    id: usuarioDB._id
+                });
+            })
+        }
+    });
+})
+
+////////////////////////////////////////////AUTENTICACION NORMAL///////////////////////////////////////////////
+
 app.post('/', (req, res) => {
 
     var body = req.body;
